fb4d7074047208f83e8c0a27e09ff24e61ff55f1
Philipp Muens <raute1337@gmx.de>
2015-09-23 21:46:45 +0200

    Add sign out functionality
    Add sign out functionality so that users can sign out of the
    application.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,4 +1,9 @@
 Header = React.createClass({
+  signOut() {
+    Meteor.logout();
+    FlowRouter.go('signIn');
+  },
+
   render() {
     const homePath = FlowRouter.path('home');
 
@@ -7,6 +12,13 @@ Header = React.createClass({
         <div className="container">
           <ul>
             <li><a href={homePath}>Scalable</a></li>
+            {Meteor.userId() ? (
+              <li className="u-pull-right">
+                <button type="button" onClick={this.signOut}>
+                  <i className="fa fa-power-off fa-lg"></i>
+                </button>
+              </li>
+            ) : null}
           </ul>
         </div>
       </header>
