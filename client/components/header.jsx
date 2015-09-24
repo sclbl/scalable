4e82c64a58b4831bb16c4844d8e74dd72d0f0239
Philipp Muens <raute1337@gmx.de>
2015-09-24 08:12:37 +0200

    Add show E-Mail address in header
    Add show E-Mail address in header so that the user knows which
    login is currently in use.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,6 +4,10 @@ Header = React.createClass({
     FlowRouter.go('signIn');
   },
 
+  emailAddress() {
+    return Meteor.user() && Meteor.user().emails && Meteor.user().emails[0].address
+  },
+
   render() {
     const homePath = FlowRouter.path('home');
 
@@ -13,11 +17,16 @@ Header = React.createClass({
           <ul>
             <li><a href={homePath}>Scalable</a></li>
             {Meteor.userId() ? (
-              <li className="u-pull-right">
-                <button type="button" onClick={this.signOut}>
-                  <i className="fa fa-power-off fa-lg"></i>
-                </button>
-              </li>
+              <div>
+                <li className="sign-out u-pull-right">
+                  <button type="button" onClick={this.signOut}>
+                    <i className="fa fa-power-off fa-lg"></i>
+                  </button>
+                </li>
+                <li className="u-pull-right">
+                  <span>{this.emailAddress()}</span>
+                </li>
+              </div>
             ) : null}
           </ul>
         </div>
