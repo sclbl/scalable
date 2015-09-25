bb69053b333161e3dd8e4045e8f03c3f9c168e19
Philipp Muens <raute1337@gmx.de>
2015-09-25 13:26:10 +0200

    Refactor setup file
    Refactor setup file so that the adminUser variable is a const (and can
    not be changed) and the logger outputs an info rather than a warn for
    the "Successfully created the admin user" message.

--- server/setup.js
+++ server/setup.js
@@ -1,10 +1,10 @@
 Meteor.startup(() => {
   // Create an admin user if not yet present
   if (Meteor.users.find().count() === 0) {
-    let adminUser = Accounts.createUser({ username: 'admin', email: 'admin@sclbl.com', password: '12345678', isAdmin: true });
+    const adminUser = Accounts.createUser({ username: 'admin', email: 'admin@sclbl.com', password: '12345678', isAdmin: true });
 
     if (adminUser) {
-      logger.warn('Successfully created the admin user.');
+      logger.info('Successfully created the admin user.');
       logger.warn('NOTE: Please change the admin users password as soon as possible!');
     } else {
       logger.error('An error occurred while creating the admin user.');
